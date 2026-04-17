bb55548f4028cc627cf382f6b94a668b7d2323bb
KuntrapakamJishnu <kjishnu973@gmail.com>
2026-04-17 17:37:07 +0530

    Send Google OAuth success flow directly to home

--- backend/src/controller/oauth.controller.js
+++ backend/src/controller/oauth.controller.js
@@ -29,7 +29,7 @@ const getGoogleRedirectUri = () => {
 
 const getFrontendLoginSuccessUrl = () => {
   const frontendBase = (ENV.FRONTEND_URL || 'http://localhost:5173').replace(/\/$/, '')
-  return `${frontendBase}/login?success=true`
+  return `${frontendBase}/`
 }
 
 export const getGoogleAuthUrl = () => {
